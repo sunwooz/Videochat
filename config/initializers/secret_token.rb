fbcd883868ac3b92e16f449363ed96aac2cda016
Daniel Yang <yangsunwoo@gmail.com>
2012-05-04 15:16:07 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Videochat::Application.config.secret_token = '64aaf18d04c94c4b222011cc7ff4e3f35ac2fcd7783f0eff21753b0665b6269ffb4d789c14b3b01939e4b752e626eded5b3954a2a1cd918a754f2263fe9706b8'
