3405987949fd8add4b2fa5329a046c0b6ee15943
Daniel Yang <yangsunwoo@gmail.com>
2012-05-05 23:24:51 -0400

    precompiled

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :name, :post_id
   
   belongs_to :post
+  
+  validates_presence_of :name, :body
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   attr_accessible :body, :title
   
   has_many :comments, :dependent => :destroy
+  
+  validates_presence_of :title, :body
 end
