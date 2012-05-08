0d6597d251766e83f56269c637ae6ba7247fbb8d
Daniel Yang <yangsunwoo@gmail.com>
2012-05-08 19:50:01 -0400

    added streamHandler

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,10 @@ class PagesController < ApplicationController
     @api_key = "14991521"        # Replace with your OpenTok API key.
     @api_secret = "df63492d334eaef0eccea97185f5ce688860f1b3"  # Replace with your OpenTok API secret.
 
-    opentok = OpenTok::OpenTokSDK.new @api_key, @api_secret
-    @session = opentok.create_session request.remote_addr
+    @opentok = OpenTok::OpenTokSDK.new @api_key, @api_secret
+    @session = @opentok.create_session request.remote_addr
 
-    token = opentok.generate_token :session_id => session, :role => OpenTok::RoleConstants::PUBLISHER, :connection_data => "username=Bob,level=4"
+    @token = @opentok.generate_token :session_id => @session, :role => OpenTok::RoleConstants::PUBLISHER, :connection_data => "username=Bob,level=4"
   end
   
   def contact
