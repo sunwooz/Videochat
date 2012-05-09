2405b3085c1d5c6d348ccba733e7c3c8ac21cac0
Daniel Yang <yangsunwoo@gmail.com>
2012-05-09 01:29:01 -0400

    new sessionid room

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def index
   end
   
-  def room
+  def newsession
     @api_key = "15025822"        # Replace with your OpenTok API key.
     @api_secret = "ffe2c7919d9364e7f8f9154eaa5f33639bcbcee6"  # Replace with your OpenTok API secret.
 
@@ -10,6 +10,13 @@ class PagesController < ApplicationController
     @session = @opentok.create_session request.remote_addr
 
     @token = @opentok.generate_token :session_id => @session, :role => OpenTok::RoleConstants::PUBLISHER, :connection_data => "username=Bob,level=4"
+  end
+  
+  def room
+    @api_key = "15025822"        # Replace with your OpenTok API key.
+    @api_secret = "ffe2c7919d9364e7f8f9154eaa5f33639bcbcee6"  # Replace with your OpenTok API secret.
+
+    @opentok = OpenTok::OpenTokSDK.new @api_key, @api_secret
   end
   
   def contact
